814cfafd473610b0077d29c806be744d4575f1a6
OlehKuryshko <oleh.kuryshko@gmail.com>
2018-05-23 23:36:59 +0300

    hw7

---
+++ js/init.js
@@ -0,0 +1,13 @@
+$( function() {
+    // init tabs
+    $( ".services-tab" ).tabs();
+    $( ".portfolio-tab" ).tabs();
+    // init portfolio tabs
+
+    //init reviews carousel
+    $('.reviews-carousel').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+} );
